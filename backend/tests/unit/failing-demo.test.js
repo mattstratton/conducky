efd631600af2c19d9391b399e07e84c7351e79f2
Matt Stratton <matt.stratton@gmail.com>
2025-06-08 23:41:54 -0500

    Remove test test

--- backend/tests/unit/failing-demo.test.js
+++
@@ -1,7 +0,0 @@
-// TEMPORARY: Remove this file after confirming CI artifact upload on test failure
-
-describe("Temporary failing test for CI workflow", () => {
-  it("should fail intentionally", () => {
-    expect(true).toBe(false);
-  });
-});
