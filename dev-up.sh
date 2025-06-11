d8477b4dd59ede8ed518abb2bfa912e017aa6100
Matt Stratton <matt.stratton@gmail.com>
2025-06-10 23:23:46 -0500

    chore: dev-up.sh runs in detached mode; release.yml only runs on tag push to main

--- dev-up.sh
+++ dev-up.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Run docker-compose with APP_VERSION set to the current git tag or commit hash
+# Containers will run in detached mode (-d). View logs with 'docker-compose logs -f'.
+# Stop all containers with 'docker-compose down'.
 
 # Try to get the latest tag, fallback to commit hash if none
 TAG=$(git describe --tags --abbrev=0 2>/dev/null)
@@ -9,4 +11,4 @@ fi
 
 export APP_VERSION=$TAG
 echo "Using APP_VERSION=$APP_VERSION"
-docker-compose up --build 
+docker-compose up --build -d 
